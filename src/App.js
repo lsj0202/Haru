b2a08b355ce4b20edffc5d5572436501a7905233
Sang jin Lee <w1004123@gmail.com>
2023-03-24 00:41:56 +0900

    NAV Change IMG

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import Navbar from './Components/Navbar';
-import { RxHamburgerMenu } from 'react-icons/rx';
+//import { RxHamburgerMenu } from 'react-icons/rx';
+import Nav from './Components/Images/burger-menu.png';
+import X from './Components/Images/x-button.png';
 import './App.css';
 
 
 const App = () => {
   const [showNav, setShowNav] = useState(false);
+  const [imageSrc, setImageSrc] = useState(Nav);
 
   const change = () => {
-    setShowNav(!showNav);
-    
+    if(showNav) {
+      setImageSrc(Nav);
+      setShowNav(false);
+    } else {
+      setImageSrc(X);
+      setShowNav(true);
+    }
   }
 
   return (
     <div className='App'>
       <header>
-        <RxHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+        <img src={imageSrc} className='logo' onClick={change} alt="img"/>
       </header>
 
       <Navbar show={showNav}/>
